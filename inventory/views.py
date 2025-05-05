8e75883e643823fd30477a7f8a8608f8df5b06f0
gabriellemosc <gabriel.palacios1910@gmail.com>
2025-05-04 21:07:10 -0300

    Finish the Send Context for the Crete Product

--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect       #render give a html page as response, redirect make the user goes to other URL
 from .forms import ItemForm, CategoryForm, SubCategoryForm
 from django.shortcuts import redirect
+from .models import Category, SubCategory
 
 # Create your views here.
 
@@ -8,25 +9,46 @@ def homepage(request):  #after the urls.py direct to here, the function decide w
     return render(request, 'inventory/homepage.html')
 
 def create_product(request):
+    #when the user register a new subcategory we get to the form
+    category_id = request.GET.get('category_id')
+    subcategory_id = request.GET.get('subcategory_id')
+
+    inital_data = {}
+#if the user already register, we try to get 
+    if category_id:
+            try:
+                inital_data['category']  = Category.objects.get(id=category_id)
+            except Category.DoesNotExist:
+                pass
+    
+    if subcategory_id:
+            try:
+                inital_data['subcategory'] = SubCategory.objects.get(id=subcategory_id)  
+            except SubCategory.DoesNotExist:
+                pass             
+
     if request.method == 'POST':        #check if was a POST
-        form = ItemForm(request.POST)   #IF yes, takes the parameter by the USER CREATE A FORM AND SAVE ON DB 
+        form = ItemForm(request.POST, request.FILES)   #IF yes, takes the parameter by the USER CREATE A FORM AND SAVE ON DB 
         if form.is_valid():
             form.save()
             return redirect('homepage')
-    else:                               #if was A GET e not a post
-        form = ItemForm()               #create the form
+    else:     
+        #when the user register subcategory, we already take 
+        form = ItemForm(initial=inital_data)               #create the form and if we have the other context give to the forms
 
     return render(request, 'inventory/Create_Product/create_product.html',{"form":form})   #send the object to the form
 
 
 def create_category(request):
     next_url = request.GET.get('next', '/')     #take the URL, beside next to save to redirect where the user was
+
     if request.method == 'POST':
         form = CategoryForm(request.POST)
         if form.is_valid():
-            form.save()
+            category =  form.save() 
             #redirect to the previous page
-            return redirect(next_url)
+            redirect_url = f"{next_url}?category_id={category.id}"  #send the ID of the category
+            return redirect(redirect_url)
     else:
             form = CategoryForm()
 
@@ -34,15 +56,16 @@ def create_category(request):
 
 
 def create_subcategory(request):
-    next_url = request.GET.get('next', '/')
+    next_url = request.GET.get('next', '/')         #take the previous path
     category_id  = request.GET.get('category_id')
 
     if request.method == 'POST':
         form = SubCategoryForm(request.POST)
         if form.is_valid():
-            form.save()
-            #redirect previous page
-            return redirect(next_url)
+            subcategory = form.save()
+            #redirect previous page with the context of the category and subcategory
+            redirect_url = f"{next_url}?category_id={subcategory.category.id}&subcategory_id={subcategory.id}" #send to the context
+            return redirect(redirect_url)
     else:
         inital_data = {}
         if category_id:
